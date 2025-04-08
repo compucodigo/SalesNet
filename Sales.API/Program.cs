76ce12bf58578bb8e1f8ec60f5a90315dc1368aa
Juan <jmph81@gmail.com>
2025-04-08 00:39:07 -0400

    API Controller Countries

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -9,19 +9,41 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));
+builder.Services.AddSwaggerGen(); 
 
 var app = builder.Build();
+//app.MapGet("/", (HttpContext context) =>
+//{
+//    context.Response.Redirect("/swagger/index.html", permanent:false);
+//});
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger/index.html", permanent: false);
+        return;
+    }
+    await next();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI();
+    app.UseSwagger();
+
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 
 app.Run();
