024204573e2c127a8472f86cb1a11a67757f4a05
Juan <jmph81@gmail.com>
2025-04-11 14:18:28 -0400

    SeedDB v1.0

---
+++ Sales.API/Data/SeedDb.cs
@@ -0,0 +1,43 @@
+﻿
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _dataContext;
+
+        public SeedDb(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _dataContext.Database.EnsureCreatedAsync();
+            await CheckCountriosAsync();
+
+        }
+
+        private async Task CheckCountriosAsync()
+        {
+            if (!_dataContext.Countries.Any())
+            {
+                _dataContext.Countries.AddRange(new List<Country>()
+                {
+                    new Country() { Name = "Brazil" },
+                    new Country() { Name = "USA" },
+                    new Country() { Name = "Canada" },
+                    new Country() { Name = "Argentina" },
+                    new Country() { Name = "Colombia" },
+                    new Country() { Name = "Chile" },
+                    new Country() { Name = "Peru" },
+                    new Country() { Name = "Venezuela" },
+                    new Country() { Name = "Ecuador" },
+                    new Country() { Name = "Paraguay" }
+                });
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -3,19 +3,26 @@ using Sales.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));
-builder.Services.AddSwaggerGen(); 
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<SeedDb>();
 
 var app = builder.Build();
-//app.MapGet("/", (HttpContext context) =>
-//{
-//    context.Response.Redirect("/swagger/index.html", permanent:false);
-//});
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopedFactory!.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+}
+
 app.Use(async (context, next) =>
 {
     if (context.Request.Path == "/")
