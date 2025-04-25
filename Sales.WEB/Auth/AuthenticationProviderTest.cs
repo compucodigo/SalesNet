40fc8758c347549a13a91a12a748e230556d4d4c
Juan <jmph81@gmail.com>
2025-04-25 11:39:32 -0400

    Secutiryon Web

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonymous = new ClaimsIdentity();
+            var juanUser = new ClaimsIdentity(new[]
+            {
+                new Claim("FirstName", "Juan"),
+                new Claim("LastName", "Perez"),
+                new Claim(ClaimTypes.Name, "jperez@yopmail.com"),
+                new Claim(ClaimTypes.Role, "User")
+            }, 
+            authenticationType: "Test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(juanUser)));
+        }
+    }
+}
